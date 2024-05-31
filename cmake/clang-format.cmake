c22af6e1e40265217b1e0f97ab868eeda086a7a1
Joseph Cosentino <jcosentino11@gmail.com>
2024-05-30 19:31:44 -0700

    test on windows and mac

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,7 +1,11 @@
-file(GLOB_RECURSE ALL_SOURCE_FILES src/*.cpp include/*.h test/*.cpp)
+find_program(CLANG_FORMAT "clang-format")
 
-add_custom_target(
-    clang-format
-    ALL
-    COMMAND clang-format -i ${ALL_SOURCE_FILES}
-)
+if (CLANG_FORMAT)
+    file(GLOB_RECURSE ALL_SOURCE_FILES src/*.cpp include/*.h test/*.cpp)
+
+    add_custom_target(
+        clang-format
+        ALL
+        COMMAND clang-format -i ${ALL_SOURCE_FILES}
+    )
+endif()
